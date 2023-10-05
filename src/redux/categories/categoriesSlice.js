21c951ed1376ee56fe8d6de0b93f6cb38a99787e
Anold Mwagharo <anoldmwawasi@gmail.com>
2023-10-05 14:38:19 +0300

    Fix eslint errors

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,21 +1,20 @@
-import {createSlice} from "@reduxjs/toolkit"
+import { createSlice } from '@reduxjs/toolkit';
 
 export const categoriesSlice = createSlice({
-  name: "categories",
+  name: 'categories',
   initialState: {
-    status: "Under construction",
+    status: 'Under construction',
     categories: [],
   },
   reducers: {
     checkStatus: (state, action) => {
-      state.categories =
-        action.payload === "Under construction"
-          ? "Under construction"
-          : state.categories
+      state.categories = action.payload === 'Under construction'
+        ? 'Under construction'
+        : state.categories;
     },
   },
-})
+});
 
-export const {checkStatus} = categoriesSlice.actions
+export const { checkStatus } = categoriesSlice.actions;
 
-export default categoriesSlice.reducer
+export default categoriesSlice.reducer;
