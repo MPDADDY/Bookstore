171f3b8c1bef5b96fae269f46de49ce3f9fc9118
Anold Mwagharo <anoldmwawasi@gmail.com>
2023-10-05 09:46:33 +0300

    Fix lint errors

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,22 +1,22 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  books:[]
-}
+  books: [],
+};
 
 const BookSlice = createSlice({
-  name: books,
+  name: 'books',
   initialState,
   reducer: {
-    added: (state,action) => {
-      state.books.push(action.payload)
+    added: (state, action) => {
+      state.books.push(action.payload);
     },
     removed: (state, action) => {
-      const itemId = action.payload
-      state.books = state.books.filter(book => book.itemId !== itemId)
-    }
-  }
-})
+      const itemId = action.payload;
+      state.books = state.books.filter((book) => book.itemId !== itemId);
+    },
+  },
+});
 
-export const {added, removed} = BookSlice.actions
-export default BookSlice.reducer
+export const { added, removed } = BookSlice.actions;
+export default BookSlice.reducer;
